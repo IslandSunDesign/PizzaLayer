d72d7d475b29694947226b205d5051716d6f2379
IslandSunDesign <info@islandsundesign.com>
2025-05-12 12:53:09 -1000

    Update topper-ui-css.php
    1. Added CSS generation for the following global options: pizzalayer_cheese_setting_cheesedistance, pizzalayer_setting_crust_aspectratio, pizzalayer_setting_crust_padding, pizzalayer_setting_advanced_css, pizzalayer_setting_pizza_size_max, pizzalayer_setting_pizza_size_min, pizzalayer_setting_pizza_border, pizzalayer_setting_pizza_border_color
    2. Removed option to set additional CSS in plugin options (setting : pizzalayer_setting_advanced_css) as unnecessary - most themes and builders have this option, and it may pose a code injection risk.

--- includes/public/topper-ui-css.php
+++ includes/public/topper-ui-css.php
@@ -3,27 +3,56 @@
 //PIZZA-SPECIFIC CSS - values applied from customizer / global meta
 
 function pizzalayer_options_css_output(){
-// static variables
+
+/* ==  Get global options == */
 $pizzalayer_global_options_string = '';
-
-// get WP plugin option values
-$pizzalayer_global_option_color = get_option( 'pizzalayer_setting_global_color' );
-if($pizzalayer_global_option_color){ $pizzalayer_global_options_string .= '.pizzalayer-ui-menu{background:' . $pizzalayer_global_option_color . ';'; };
-
 $pizzalayer_global_option_cheese_distance = get_option( 'pizzalayer_cheese_setting_cheesedistance' );
-if($pizzalayer_global_option_cheese_distance){ $pizzalayer_global_options_string .= '#pizzalayer-base-layer-sauce{padding:' . $pizzalayer_global_option_cheese_distance . ';'; };
-
 $pizzalayer_global_option_crust_aspectratio = get_option( 'pizzalayer_setting_crust_aspectratio' );
-if($pizzalayer_global_option_crust_aspectratio){ $pizzalayer_global_options_string .= '#pizzalayer-base-layer-sauce{aspect-ratio:' . $pizzalayer_global_option_crust_aspectratio . ';'; };
-
+$pizzalayer_global_option_crust_padding = get_option( 'pizzalayer_setting_crust_padding' );
 $pizzalayer_global_option_advanced_css = get_option( 'pizzalayer_setting_advanced_css' );
+$pizzalayer_global_option_color = get_option( 'pizzalayer_setting_global_color' );
+
+$pizzalayer_template_glass_option_pizza_size_max = get_option( 'pizzalayer_setting_pizza_size_max' );
+$pizzalayer_template_glass_option_pizza_size_min = get_option( 'pizzalayer_setting_pizza_size_min' );
+$pizzalayer_template_glass_option_pizza_border = get_option( 'pizzalayer_setting_pizza_border' );
+$pizzalayer_template_glass_option_pizza_border_color = get_option( 'pizzalayer_setting_pizza_border_color' );
+
+/* ==  Generate CSS for inline placement based on global option values == */
+
+if($pizzalayer_global_option_color){ $pizzalayer_global_options_string .= '.pizzalayer-ui-menu{background:' . $pizzalayer_global_option_color . ';}'; };
+
+if($pizzalayer_global_option_cheese_distance){ $pizzalayer_global_options_string .= '
+#pizzalayer-base-layer-sauce{padding:' . $pizzalayer_global_option_cheese_distance . ';}'; };
+
+if($pizzalayer_global_option_crust_aspectratio){ $pizzalayer_global_options_string .= '
+#pizzalayer-base-layer-sauce{aspect-ratio:' . $pizzalayer_global_option_crust_aspectratio . ';}'; };
+
+if($pizzalayer_global_option_crust_padding){ $pizzalayer_global_options_string .= '
+#pizzalayer-base-layer-crust{padding:' . $pizzalayer_global_option_crust_padding . 'px !important;}'; };
+
+if($pizzalayer_template_glass_option_pizza_size_min){ $pizzalayer_global_options_string .= '
+#pizzalayer-pizza{min-width:' . $pizzalayer_template_glass_option_pizza_size_min . ';min-height:' . $pizzalayer_template_glass_option_pizza_size_min . ';}'; };
+
+if($pizzalayer_template_glass_option_pizza_size_max){ $pizzalayer_global_options_string .= '
+#pizzalayer-pizza{max-width:' . $pizzalayer_template_glass_option_pizza_size_max . ';}'; };
+
+if($pizzalayer_template_glass_option_pizza_border_color && $pizzalayer_template_glass_option_pizza_border){
+    $pizzalayer_global_options_string .= '
+    body .pizzalayer-crust{border:solid ' . $pizzalayer_template_glass_option_pizza_border . ' ' . $pizzalayer_template_glass_option_pizza_border_color . ';}';
+} else if($pizzalayer_template_glass_option_pizza_border){ 
+    $pizzalayer_global_options_string .= '
+    body .pizzalayer-crust{border:solid ' . $pizzalayer_template_glass_option_pizza_border . ' #FFFFFF;}'; 
+};
+
 if($pizzalayer_global_option_advanced_css){ $pizzalayer_global_options_string .= $pizzalayer_global_option_advanced_css; };
 
-//gather CSS for 
-$pizzalayer_options_css = '<style type="text/css">' . $pizzalayer_global_options_string . '</style>';
+/* ==  Wrap CSS in style tags == */
+$pizzalayer_options_css = '<style>
+/*-- Dynamic CSS from topper-ui-css.php --*/
+' . $pizzalayer_global_options_string . '
+</style>';
 
-//finally, return combined CSS
+/* ==  finally, return combined CSS == */
 return $pizzalayer_options_css;
-    
-    
+
 } //close function
