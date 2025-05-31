6308752e8562ec477e121c6ecf876b9b921154a7
IslandSunDesign <info@islandsundesign.com>
2025-05-30 19:10:35 -1000

    Update topper-ui-pizza-builder.php
    1. Expanded parameters available for pizzalayer_pizza_dynamic_nested() to include all elements for setting defaults or passing variables
    2. reworked how each layer is prepared for sending to the pizzalayer_layer_nest() function for being able to set defaults using the plugin options
    3. removed a commented dev note (line 108)

--- includes/builder/topper-ui-pizza-builder.php
+++ includes/builder/topper-ui-pizza-builder.php
@@ -1,6 +1,4 @@
 <?php
-global $pizzalayer_path_images;
-
 // Function to get WordPress post ID given the post title
 function pizzalayer_getIdBySlug( $slug, $posttype ){
    $layer = get_page_by_path($slug,OBJECT,'pizzalayer_' . $posttype);
@@ -8,47 +6,79 @@ function pizzalayer_getIdBySlug( $slug, $posttype ){
 }
 
 
+
 /* ============================================= 
 PIZZALAYER : DYNAMIC NESTED CUSTOM PIZZA DISPLAY - used for showing a buildable pizza on a page or post */
 function pizzalayer_pizza_dynamic_nested(
-    $ptpizza_dynamic_pizza_id = '',
-    $ptpizza_dynamic_pizza_crust = 'plain',
-    $ptpizza_dynamic_pizza_sauce = 'sauce-red-medium',
-    $ptpizza_dynamic_pizza_toppings_array = ''){
+    $ptpizza_dynamic_pizza_id = 'pizza-dynamic',
+    $ptpizza_dynamic_pizza_css_id = 'pizza-dynamic-id',
+    $ptpizza_dynamic_pizza_crust = '',
+    $ptpizza_dynamic_pizza_sauce = '',
+    $ptpizza_dynamic_pizza_cheese = '',
+    $ptpizza_dynamic_pizza_toppings_array = '',
+    $ptpizza_dynamic_pizza_drizzle = '',
+    $ptpizza_dynamic_pizza_cut = ''
+    ){
+     
+// GET LAYER PRESETS FROM PLUGIN OPTIONS
+
+
 // PREPARE CRUST AS OUTERMOST LAYER
-global $pizzalayer_path_images;
-$ptpizza_layer_crust = pizzalayer_layer_nest(200,'crust', $pizzalayer_path_images . 'plain.png','plain crust');    
+$ptpizza_layer_crust_option = get_option('pizzalayer_setting_crust_defaultcrust');
+if($ptpizza_layer_crust_option != ''){ $ptpizza_dynamic_pizza_crust = $ptpizza_layer_crust_option; };
+$ptpizza_layer_crust_id = pizzalayer_getIdBySlug($ptpizza_dynamic_pizza_crust,'crusts');
+$ptpizza_layer_crust = pizzalayer_layer_nest(100,'crust', get_field('crust_layer_image', $ptpizza_layer_crust_id), $ptpizza_dynamic_pizza_crust);    
     
 // PREPARE SAUCE AS 2ND LAYER
-$ptpizza_layer_sauce = pizzalayer_layer_nest(200,'sauce', '','pizza sauce - medium red sauce');    
+$ptpizza_layer_sauce_option = get_option('pizzalayer_setting_sauce_defaultsauce');
+if($ptpizza_layer_sauce_option != ''){ $ptpizza_dynamic_pizza_sauce = $ptpizza_layer_sauce_option; };
+$ptpizza_layer_sauce_id = pizzalayer_getIdBySlug($ptpizza_dynamic_pizza_sauce,'sauces');
+$ptpizza_layer_sauce = pizzalayer_layer_nest(150,'sauce', get_field('sauce_layer_image', $ptpizza_layer_sauce_id), $ptpizza_dynamic_pizza_sauce);     
 
 // PREPARE CHEESE AS 3RD LAYER
-$ptpizza_layer_cheese = pizzalayer_layer_nest(300,'cheese', '','mozzarella cheese - melted');
+$ptpizza_layer_cheese_option = get_option('pizzalayer_setting_cheese_defaultcheese');
+if($ptpizza_layer_cheese_option != ''){ $ptpizza_dynamic_pizza_cheese = $ptpizza_layer_cheese_option; };
+$ptpizza_layer_cheese_id = pizzalayer_getIdBySlug($ptpizza_dynamic_pizza_cheese,'cheeses');
+$ptpizza_layer_cheese = pizzalayer_layer_nest(200,'cheese', get_field('cheese_layer_image', $ptpizza_layer_cheese_id), $ptpizza_dynamic_pizza_cheese); 
 
 // PREPARE DRIZZLE AS 5TH LAYER
-$ptpizza_layer_drizzle = pizzalayer_layer_nest(700,'drizzle', '','mozzarella cheese - melted') . '</div>';
+$ptpizza_layer_drizzle_option = get_option('pizzalayer_setting_drizzle_defaultdrizzle');
+if($ptpizza_layer_drizzle_option != ''){ $ptpizza_dynamic_pizza_drizzle = $ptpizza_layer_drizzle_option; };
+$ptpizza_layer_drizzle_id = pizzalayer_getIdBySlug($ptpizza_dynamic_pizza_drizzle,'drizzles');
+$ptpizza_layer_drizzle = pizzalayer_layer_nest(900,'drizzle', get_field('drizzle_layer_image', $ptpizza_layer_drizzle_id), $ptpizza_dynamic_pizza_drizzle); 
 
 // ADD PIZZA CUT DIAGRAM AS TOP LAYER
-$ptpizza_layer_cut = pizzalayer_layer_nest(950,'cut', '','Slices') . '</div>';
-
+$ptpizza_layer_cut_option = get_option('pizzalayer_setting_cut_defaultcut');
+if($ptpizza_layer_cut_option != ''){ $ptpizza_dynamic_pizza_cut = $ptpizza_layer_cut_option; };
+$ptpizza_layer_cut_id = pizzalayer_getIdBySlug($ptpizza_dynamic_pizza_cut,'cuts');
+$ptpizza_layer_cut = pizzalayer_layer_nest(950,'cut', get_field('cut_layer_image', $ptpizza_layer_cut_id), $ptpizza_dynamic_pizza_cut); 
 
 // PREPARE TOPPINGS AS LAYER
 $pizzalayer_this_toppings_array = explode(',',$ptpizza_dynamic_pizza_toppings_array); //convert toppings string into an array
 $ptpizza_layer_index = 400; //layer # (z-index)
 $ptpizza_toppings_output_html = '<div id="pizzalayer-toppings-wrapper" class="pizzalayer-toppings-wrapper pizzalayer-toppings-wrapper-dynamic">';
+
 //START PARSING TOPPINGS - START LOOPING THROUGH ARRAY VALUES
 foreach ($pizzalayer_this_toppings_array as $pizzalayer_this_layer) {
-$ptpizza_layer_index = $ptpizza_layer_index + 40; // topping layer (z-index value)
+$ptpizza_layer_index += 5; // topping layer (z-index value)
 $ptpizza_layer_slug = $pizzalayer_this_layer; // topping slug
-$ptpizza_layer_imageurl = $pizzalayer_path_images . 'toppings/' . $ptpizza_layer_slug . '.png'; // generate topping image path from slug
+$ptpizza_layer_id = pizzalayer_getIdBySlug($ptpizza_layer_slug,'toppings');
+$ptpizza_layer_imageurl = get_field('topping_layer_image', $ptpizza_layer_id);
 $ptpizza_layer_alt = 'Pizza topping : ' . $ptpizza_layer_slug; // topping alt / description
+
 //PREPARE PIZZA LAYER FROM CURRENT ARRAY ITEM
-$ptpizza_toppings_output_html .= pizzalayer_layer($ptpizza_layer_index, $ptpizza_layer_slug, $ptpizza_layer_imageurl, $ptpizza_layer_alt);
+$ptpizza_toppings_output_html .= pizzalayer_layer( $ptpizza_layer_index, $ptpizza_layer_slug, $ptpizza_layer_imageurl, $ptpizza_layer_alt );
 }; // END foreach
 
-//BAKE THE PIZZA HTML USING COMPILED STRING
-$pt_pizza_cooked = pizzalayer_ui_wrapper_pizza_dyn_1_start() . $ptpizza_layer_crust . $ptpizza_layer_sauce . $ptpizza_layer_cheese . $ptpizza_toppings_output_html . '
-</div><!-- // close pizza toppings wrapper -->' . $ptpizza_layer_drizzle . $ptpizza_layer_cut . '
+// PREPARE DRIZZLE AS 5TH LAYER
+$ptpizza_layer_drizzle = pizzalayer_layer_nest(950,'drizzle', '','drizzle') . '</div>';
+
+// ADD PIZZA CUT DIAGRAM AS TOP LAYER
+$ptpizza_layer_cut = pizzalayer_layer_nest(990,'cut', '','Slices') . '</div>';
+
+//BAKE THE PIZZA HTML AND CLOSE NESTED CONTAINERS
+$pt_pizza_cooked = pizzalayer_ui_wrapper_pizza_dyn_1_start() . $ptpizza_layer_crust . $ptpizza_layer_sauce . $ptpizza_layer_cheese . $ptpizza_toppings_output_html . $ptpizza_layer_drizzle . '
+</div><!-- // close pizza toppings wrapper -->' . $ptpizza_layer_cut . '
 </div><!-- // close pizza cheese -->
 </div><!-- // close pizza sauce -->
 </div><!-- // close pizza crust -->
@@ -67,33 +97,50 @@ PIZZALAYER : STATIC NESTED CUSTOM PIZZA DISPLAY - used for showing a static pizz
 function pizzalayer_pizza_static_nested(
     $ptpizza_static_pizza_id = 'pizza-static',
     $ptpizza_static_pizza_css_id = 'pizza-static-id',
-    $ptpizza_static_pizza_crust = 'hand-tossed-wheat-crust',
-    $ptpizza_static_pizza_sauce = 'red-sauce',
-    $ptpizza_static_pizza_cheese = 'ricotta',
-    $ptpizza_static_pizza_toppings_array = 'onion-rings,pineapple-slices,tomatoes',
-    $ptpizza_static_pizza_drizzle = 'ranch-dressing',
-    $ptpizza_static_pizza_cut = '8-slices'
+    $ptpizza_static_pizza_crust = '',
+    $ptpizza_static_pizza_sauce = '',
+    $ptpizza_static_pizza_cheese = '',
+    $ptpizza_static_pizza_toppings_array = '',
+    $ptpizza_static_pizza_drizzle = '',
+    $ptpizza_static_pizza_cut = ''
     ){
+        
+        
+        
 
 // PREPARE CRUST AS OUTERMOST LAYER
+$ptpizza_layer_crust_option = get_option('pizzalayer_setting_crust_defaultcrust');
+if($ptpizza_layer_crust_option != ''){ $ptpizza_static_pizza_crust = $ptpizza_layer_crust_option; };
 $ptpizza_layer_crust_id = pizzalayer_getIdBySlug($ptpizza_static_pizza_crust,'crusts');
 $ptpizza_layer_crust = pizzalayer_layer_nest(200,'crust', get_field('crust_layer_image', $ptpizza_layer_crust_id), $ptpizza_static_pizza_crust);    
     
 // PREPARE SAUCE AS 2ND LAYER
+$ptpizza_layer_sauce_option = get_option('pizzalayer_setting_sauce_defaultsauce');
+if($ptpizza_layer_sauce_option != ''){ $ptpizza_static_pizza_sauce = $ptpizza_layer_sauce_option; };
 $ptpizza_layer_sauce_id = pizzalayer_getIdBySlug($ptpizza_static_pizza_sauce,'sauces');
-$ptpizza_layer_sauce = pizzalayer_layer_nest(200,'sauce', get_field('sauce_layer_image', $ptpizza_layer_sauce_id), $ptpizza_static_pizza_sauce);     
+$ptpizza_layer_sauce = pizzalayer_layer_nest(250,'sauce', get_field('sauce_layer_image', $ptpizza_layer_sauce_id), $ptpizza_static_pizza_sauce);     
 
 // PREPARE CHEESE AS 3RD LAYER
+$ptpizza_layer_cheese_option = get_option('pizzalayer_setting_cheese_defaultcheese');
+if($ptpizza_layer_cheese_option != ''){ $ptpizza_static_pizza_cheese = $ptpizza_layer_cheese_option; };
 $ptpizza_layer_cheese_id = pizzalayer_getIdBySlug($ptpizza_static_pizza_cheese,'cheeses');
 $ptpizza_layer_cheese = pizzalayer_layer_nest(300,'cheese', get_field('cheese_layer_image', $ptpizza_layer_cheese_id), $ptpizza_static_pizza_cheese); 
 
 // PREPARE DRIZZLE AS 5TH LAYER
+$ptpizza_layer_drizzle_option = get_option('pizzalayer_setting_drizzle_defaultdrizzle');
+if($ptpizza_layer_drizzle_option != ''){ $ptpizza_static_pizza_drizzle = $ptpizza_layer_drizzle_option; };
 $ptpizza_layer_drizzle_id = pizzalayer_getIdBySlug($ptpizza_static_pizza_drizzle,'drizzles');
-$ptpizza_layer_drizzle = pizzalayer_layer_nest(400,'drizzle', get_field('drizzle_layer_image', $ptpizza_layer_drizzle_id), $ptpizza_static_pizza_drizzle); 
+$ptpizza_layer_drizzle = pizzalayer_layer_nest(990,'drizzle', get_field('drizzle_layer_image', $ptpizza_layer_drizzle_id), $ptpizza_static_pizza_drizzle); 
 
 // ADD PIZZA CUT DIAGRAM AS TOP LAYER
+$ptpizza_layer_cut_option = get_option('pizzalayer_setting_cut_defaultcut');
+if($ptpizza_layer_cut_option != ''){ $ptpizza_static_pizza_cut = $ptpizza_layer_cut_option; };
 $ptpizza_layer_cut_id = pizzalayer_getIdBySlug($ptpizza_static_pizza_cut,'cuts');
 $ptpizza_layer_cut = pizzalayer_layer_nest(950,'cut', get_field('cut_layer_image', $ptpizza_layer_cut_id), $ptpizza_static_pizza_cut); 
+
+
+
+
 
 // USING TOPPINGS FROM SHORTCODE PARAMETERS, PREPARE TOPPINGS AS LAYERS
 $pizzalayer_this_toppings_array = explode(',',$ptpizza_static_pizza_toppings_array); //convert toppings string into an array
@@ -105,7 +152,6 @@ $ptpizza_layer_index = $ptpizza_layer_index + 10; // topping layer (z-index valu
 $ptpizza_layer_slug = $pizzalayer_this_layer; // topping slug
 $ptpizza_layer_id = pizzalayer_getIdBySlug($ptpizza_layer_slug,'toppings');
 $ptpizza_layer_imageurl = get_field('topping_layer_image', $ptpizza_layer_id);
-//$ptpizza_layer_imageurl = $pizzalayer_path_images . 'toppings/' . $ptpizza_layer_slug . '.png'; // generate topping image path from slug
 $ptpizza_layer_alt = 'Pizza topping : ' . $ptpizza_layer_slug; // topping alt / description
 //PREPARE PIZZA LAYER FROM CURRENT ARRAY ITEM
 $ptpizza_toppings_output_html .= pizzalayer_layer($ptpizza_layer_index, $ptpizza_layer_slug, $ptpizza_layer_imageurl, $ptpizza_layer_alt);
