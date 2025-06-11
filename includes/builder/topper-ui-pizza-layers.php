f335ad8bb50b3c49a076dcd96167aed62ca6e229
IslandSunDesign <info@islandsundesign.com>
2025-06-10 18:53:35 -1000

    Update topper-ui-pizza-layers.php
    1. Added do_action points
    2. Removed unncessary comment/line spacing

--- includes/builder/topper-ui-pizza-layers.php
+++ includes/builder/topper-ui-pizza-layers.php
@@ -3,17 +3,22 @@
 PIZZALAYER : TOPPING LAYERS 
 example usage: pizzalayer_layer(250,'sauce','crusts/plain.png','plain crust')
 */
-
+do_action( 'pizzalayer_file_topper-ui-pizza-layers_before' );
 
 //outputs complete layers with closing div tags
 function pizzalayer_layer( $layer_index, $layer_short, $layer_imagepath, $layer_alt ){
 global $pizzalayer_path_images;
+do_action( '' );
 return '<div id="pizzalayer-topping-' . $layer_short . '" class="pizzalayer-' . $layer_short . ' pizzalayer-topping-' . $layer_short . ' pizzalayer-layer-closed" style="z-index:' . $layer_index . ';"><img src="' . $layer_imagepath . '" id="pizzalayer-' . $layer_short . '-image" class="pizzalayer-' . $layer_short . '-image" alt="' . $layer_alt . '" title="' . $layer_alt . '" style="z-index:' . $layer_index . ';" /></div>';
 }
 
 
 //outputs nested layer without closing div tag for the three base layers : crust, sauce, and cheese. Important - You need to close these open div tags if using a custom template or code.
 function pizzalayer_layer_nest( $layer_index, $layer_short, $layer_imagepath, $layer_alt ){
+    do_action( '' );
 global $pizzalayer_path_images;
+do_action( '' );
 return '<div id="pizzalayer-base-layer-' . $layer_short . '" class="pizzalayer-' . $layer_short . ' pizzalayer-topping-' . $layer_short . ' pizzalayer-layer-nested" style="z-index:' . $layer_index . ';background: url(\'' . $layer_imagepath . '\') no-repeat;">';
+do_action( 'pizzalayer_file_topper-ui-pizza-layers_after' );
 }
+
