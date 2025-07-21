067fac3e1e66f908f03c42d5ce82a345b04ccc78
IslandSunDesign <info@islandsundesign.com>
2025-07-20 18:09:57 -1000

    Create debugging.php
    added debugging.php for inclusion of debugging and error logging functions

---
+++ includes/admin/debugging.php
@@ -0,0 +1,10 @@
+<?php
+function write_log($log) {
+    if (true === WP_DEBUG) {
+        if (is_array($log) || is_object($log)) {
+            error_log(print_r($log, true));
+        } else {
+            error_log($log);
+        }
+    }
+}
